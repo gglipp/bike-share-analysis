08c5f7a563a562e4b403422654d7455818e74ba3
panlijun <panlijun@vip.126.com>
2018-02-27 00:12:08 +0800

    update

---
+++ test2.py
@@ -0,0 +1,8 @@
+week = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday':5 , 'Sunday': 6}
+for day in week:
+    print(day)
+    print(week[day])
+    print(week.keys())
+    print(week.values())
+
+print(len(week.keys()))
